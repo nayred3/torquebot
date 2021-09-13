352e86c686349bf44865dd559b2095037a4ece99
nayred3 <68794248+nayred3@users.noreply.github.com>
2021-09-13 16:59:04 -0500

    logged in bot and added one listener to test if work
    Co-authored-by: StickmanZh <StickmanZh@users.noreply.github.com>

--- src/main/java/org/texastorque/TorqueBot.java
+++ src/main/java/org/texastorque/TorqueBot.java
@@ -2,17 +2,28 @@ package org.texastorque;
 
 // Example import
 import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
 
 /**
  * The main class for the TorqueBot.
  * 
- * @author Put ur name here  
+ * @author Suhas, Sri, Dawson 
  */
 public class TorqueBot {
     public static void main(String[] args) {
         System.out.println("TorqueBot");
         // Main method
+
+        DiscordApi api = new DiscordApiBuilder()
+            .setToken("ODg0NDg2MTI5MDY2MzQ0NDYw.YTZL5g.LyacHL1xB8B69OjGVxKTSdkNCqY")
+            .login().join();
+
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("I work!");
+            }
+        });
     }
 }
 
-/*_*/   // Fred
+/*_*/   // Fred 
