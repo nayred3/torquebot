5468e2036f6e57a32b1b229d90b9b912a437ec81
Suhas44 <suhas8@outlook.com>
2021-09-14 16:26:21 -0500

    commit

--- src/main/java/org/texastorque/TorqueBot.java
+++ src/main/java/org/texastorque/TorqueBot.java
@@ -7,16 +7,14 @@ import org.javacord.api.DiscordApiBuilder;
 /**
  * The main class for the TorqueBot.
  * 
- * @author Suhas, Sri, Dawson 
+ * @author Suhas, Sri, Dawson
  */
 public class TorqueBot {
     public static void main(String[] args) {
         System.out.println("TorqueBot");
         // Main method
 
-        DiscordApi api = new DiscordApiBuilder()
-            .setToken(Credentials.token)
-            .login().join();
+        DiscordApi api = new DiscordApiBuilder().setToken(Credentials.APITOKEN).login().join();
 
         api.addMessageCreateListener(event -> {
             if (event.getMessageContent().equalsIgnoreCase("!ping")) {
@@ -26,4 +24,4 @@ public class TorqueBot {
     }
 }
 
-/*_*/   // Fred 
+/* _ */ // Fred
