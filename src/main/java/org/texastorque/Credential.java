958ec12deb46722bd36c5495a71f735f970fd6c0
nayred3 <68794248+nayred3@users.noreply.github.com>
2021-09-14 18:34:18 -0500

    liveshare session 9/14/21

---
+++ src/main/java/org/texastorque/Credential.java
@@ -0,0 +1,7 @@
+package org.texastorque;
+
+public class Credential {
+    public static String token = "ODg0NDg2MTI5MDY2MzQ0NDYw.YTZL5g.195soTj_1C1MYffygnmUL3RXDrM";
+    public static String banwords[] = new String[] {"fuck", "bitch", "piss", "shut up", "nig"};
+    p
+}
---
+++ src/main/java/org/texastorque/DontGoHere.java
@@ -0,0 +1,3 @@
+package org.texastorque;
+
+public class 
